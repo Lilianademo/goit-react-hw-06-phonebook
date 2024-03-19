a9985999662a8d6eefc61325303eb58263c0dba1
Lilianademo <123247173+Lilianademo@users.noreply.github.com>
2024-03-19 17:32:53 +0200

    pbook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { ContactForm } from "./ContactForm/ContactForm";
+import { ContactList } from "./ContactList/ContactList";
+import { Filter } from "./Filter/Filter";
+import style from "./App.module.css";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <section className={style.container}>
+      <h1 className={style.title}>Phonebook</h1>
+      <ContactForm></ContactForm>
+      <h1 className={style.subtitle}>Contacts</h1>
+      <Filter></Filter>
+      <ContactList></ContactList>
+    </section>
   );
 };
